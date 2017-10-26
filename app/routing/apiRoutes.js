c5c9d192a535dea5c96966f6f86ff0dfb9053819
ruthieirl <ruthfromjersey@gmail.com>
2017-10-26 16:03:14 -0400

    button on home page works

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,20 @@
+//Set dependent packages
 var path = require("path");
 var friends = require("../data/friends.js");
 
+//Routes
 module.exports = function(app) {
 
+	//create an api.get route to display JSON of all possible friends
+	app.get("api/friends", function(req, res) {
+		res.JSON(friends);
+		console.log(friends);
+	});
 
+	//create an api.post route to handle incoming survey results
+	//handle compatibility logic here
+	app.post("api/friends", function(req, res) {
+
+	});
 
 };
