ca595dd2dab78ebf9a1dc29f1092a0def30e1ac5
ruthieirl <ruthfromjersey@gmail.com>
2017-10-26 09:31:13 -0400

    server.js ok

---
+++ server.js
@@ -0,0 +1,21 @@
+//Set ups dependency packages
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+//Create an express server
+var app = express();
+var port = 3000;
+
+//Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+//Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
