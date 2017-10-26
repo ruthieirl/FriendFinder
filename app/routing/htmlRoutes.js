a06716a66b9879f4f400931ac472abd80be68e68
ruthieirl <ruthfromjersey@gmail.com>
2017-10-26 12:45:26 -0400

    html routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,13 @@
+var path = require("path");
+
+module.exports = function(app) {
+
+	app.get("/home", function(req, res) {
+		res.sendFile(path.join(__dirname, "home.html"));
+	});
+
+	app.get("/survey", function(req, res) {
+		res.sendFile(path.join(__dirname, "survey.html"));
+	});
+
+};
